2c1416cd65633fb64df0885d42c9e306076c262b
Sandeep-0404 <sandeepgh2002@gmail.com>
2021-02-17 19:37:26 +0530

    Created Sign In and Sign Up layouts with firebase Authentication(EmailId and password)

--- app/src/main/java/com/example/dictionaryapp/login_signup.java
+++ app/src/main/java/com/example/dictionaryapp/login_signup.java
@@ -1,17 +1,101 @@
 package com.example.dictionaryapp;
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class login_signup extends AppCompatActivity {
+
+    private TextView newusertext;
+     EditText Emailid,pass;
+    private FirebaseAuth mAuth;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login_signup);
+
+
+        newusertext =(TextView) findViewById(R.id.newuser);
+        Emailid =(EditText) findViewById(R.id.emailsandy1);
+        pass =(EditText) findViewById(R.id.passwordsandy);
+        progressBar =(ProgressBar) findViewById(R.id.progressbarlogin);
+        mAuth = FirebaseAuth.getInstance();
+        newusertext.setPaintFlags(newusertext.getPaintFlags()| Paint.UNDERLINE_TEXT_FLAG);
+    }
+
+    public void newuser_signup(View view){
+        Intent intent =new Intent(this,signup_first.class);
+        startActivity(intent);
+    }
+
+    public void login(View view) {
+        String email = Emailid.getText().toString().trim();
+        String  password = pass.getText().toString().trim();
+
+        if (email.isEmpty()){
+            this.Emailid.setError("Field can't be empty");
+            this.Emailid.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            this.Emailid.setError("Please enter a valid email");
+            this.Emailid.requestFocus();
+            return;
+        }
+
+        if (password.isEmpty()){
+            pass.setError("field required");
+            pass.requestFocus();
+            return;
+        }
+
+        if(password.length()<6){
+            pass.setError("minimum length of passwword is 6 leter");
+            pass.requestFocus();
+        }
+
+
+
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressBar.setVisibility(View.GONE);
+                if (task.isSuccessful()){
+                    Intent intent = new Intent(getApplicationContext(), dashboard.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    public void forgetpassword(View view) {
+
     }
 }
---
+++ app/src/main/java/com/example/dictionaryapp/signup_first.java
@@ -0,0 +1,120 @@
+package com.example.dictionaryapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+
+public class signup_first extends AppCompatActivity {
+
+    private TextView create;
+    private EditText username ,email, password;
+    ProgressBar progressbar;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_signup_first);
+
+        create =(TextView)findViewById(R.id.createaccount);
+        create.setPaintFlags(create.getPaintFlags()| Paint.UNDERLINE_TEXT_FLAG);
+        username =(EditText) findViewById(R.id.usernamesandy);
+        email =(EditText) findViewById(R.id.emailsandy);
+        password =(EditText) findViewById(R.id.passwordsandy);
+        progressbar =(ProgressBar) findViewById(R.id.progressbarlogin);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+
+
+
+    //Button clicked----
+    public void goto_otp(View view) {
+
+       String usernametext = username.getText().toString().trim();
+       String emailtext = email.getText().toString().trim();
+       String passwordtext = password.getText().toString().trim();
+
+       // ALL THE VALIDATIONS-----
+
+
+        //EMAIL VALIDATIONS--
+        if (emailtext.isEmpty()){
+            this.email.setError("Field can't be empty");
+            this.email.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(emailtext).matches()){
+            this.email.setError("Please enter a valid email");
+            this.email.requestFocus();
+            return;
+        }
+
+
+        //PASSWORD VALIDATIONS----
+
+        if (passwordtext.isEmpty()){
+            password.setError("field required");
+            password.requestFocus();
+            return;
+        }
+
+        if(passwordtext.length()<6){
+            password.setError("minimum length of passwword is 6 leter");
+            password.requestFocus();
+        }
+
+
+        //USERNAME VALIADTIONS----
+        if(usernametext.isEmpty()){
+            username.setError("Field can't be Empty");
+            username.requestFocus();
+            return;
+        }
+        progressbar.setVisibility(View.VISIBLE);
+
+
+
+        mAuth.createUserWithEmailAndPassword(emailtext, passwordtext).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task){
+                progressbar.setVisibility(View.GONE);
+
+                if (task.isSuccessful()){
+                    Toast.makeText(getApplicationContext(),"User Registered Succesfully",Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(getApplicationContext(), dashboard.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+
+                }
+                else {
+                    if(task.getException() instanceof FirebaseAuthUserCollisionException){
+                        Toast.makeText(getApplicationContext(),"You are already registered",Toast.LENGTH_SHORT).show();
+                    }else {
+                        Toast.makeText(getApplicationContext(), "Some error occured", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+
+    }
+}
+
